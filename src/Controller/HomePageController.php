b91eea0855b292643ed190b1e26be1950f1501d8
Luanalgm <luanalgm05@gmail.com>
2026-01-15 12:12:28 +0100

    mise a jour homepage controller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -13,8 +13,40 @@ final class HomePageController extends AbstractController
     #[Route('/', name: 'homePage')]
     public function index(MesureRepository $mesureRepo, UserRepository $userRepo): Response
     {
+        $user = $userRepo->find(1);
+        $derniereMesure = null;
+        $historiqueData = [];
+
+        if ($user) {
+            // 1. On garde ta logique pour la boîte "Dernière mesure"
+            $derniereMesure = $mesureRepo->findOneBy(
+                ['utilisateur' => $user],
+                ['dateSaisie' => 'DESC']
+            );
+
+            // 2. On récupère TOUTES les mesures pour les graphiques (triées par date)
+            $toutesLesMesures = $mesureRepo->findBy(
+                ['utilisateur' => $user],
+                ['dateSaisie' => 'ASC']
+            );
+
+            // 3. On prépare le tableau pour JavaScript
+            foreach ($toutesLesMesures as $m) {
+                $historiqueData[] = [
+                    'date' => $m->getDateSaisie()->format('Y-m-d H:i'),
+                    'gh' => $m->getGh(),
+                    'ph' => $m->getPh(),
+                    'kh' => $m->getKh(),
+                    'nitrites' => $m->getNitrites(),
+                    'ammonium' => $m->getAmmonium(),
+                ];
+            }
+        }
+
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'mesure' => $derniereMesure,
+            // On transforme le tableau PHP en texte JSON pour le JS
+            'chartData' => json_encode($historiqueData),
         ]);
     }
 }
