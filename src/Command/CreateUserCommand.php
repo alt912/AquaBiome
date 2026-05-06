30800b5f617a5f82feb70f13b40f6ffde610cf54
RomainBoulay14 <romainboul14@yahoo.com>
2026-05-06 16:37:13 +0200

    Fix: Centering login box and update CreateUserCommand for Admin support

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -30,6 +31,7 @@ class CreateUserCommand extends Command
         $this
             ->addArgument('email', InputArgument::REQUIRED, 'User email')
             ->addArgument('password', InputArgument::REQUIRED, 'User password')
+            ->addOption('admin', 'a', InputOption::VALUE_NONE, 'Set the user as admin')
         ;
     }
 
@@ -41,7 +43,7 @@ class CreateUserCommand extends Command
 
         $user = new User();
         $user->setEmail($email);
-        $user->setRoles(['ROLE_USER']);
+        $user->setRoles($input->getOption('admin') ? ['ROLE_ADMIN'] : ['ROLE_USER']);
         
         $hashedPassword = $this->passwordHasher->hashPassword(
             $user,
