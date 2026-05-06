0c6c13b4ffc984a1d1328455951bfda77a79e3a1
RomainBoulay14 <romainboul14@yahoo.com>
2026-05-06 15:44:13 +0200

    Fix: Robust trusted proxies handling for Alwaysdata with dynamic REMOTE_ADDR

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 $_SERVER['HTTPS'] = 'on';
+$_SERVER['TRUSTED_PROXIES'] = $_SERVER['TRUSTED_PROXIES'] ?? $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1, 127.0.0.2';
+$_SERVER['TRUSTED_HEADERS'] = 'x-forwarded-for,x-forwarded-host,x-forwarded-proto,x-forwarded-port,x-forwarded-prefix';
 
 
 use App\Kernel;
